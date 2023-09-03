42d1988669c2f925361abb22e69f0fdb63de9f6f
imamhossainroni <imamhossainroni95@gmail.com>
2023-09-03 19:56:51 +0600

    ✅ setup.py file added to publish in pypi :)

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Imam Hossain Roni'
+
+# Read the long description from README.md
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="PyKite",
+    version="0.1.0",
+    description="A Python-centric micro-framework for building web applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Imam Hossain Roni",
+    author_email="imamhossainroni95@gmail.com",
+    url="https://github.com/imamhossainroni/pykite",
+    packages=find_packages(),
+    install_requires=[
+        "webob",
+        "parse",
+        "werkzeug",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pykite = pykite.__main__:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/imamhossainroni/pykite/issues",
+        "Source Code": "https://github.com/imamhossainroni/pykite",
+        "Documentation": "https://github.com/imamhossainroni/pykite/blob/main/README.md",
+        "Icon Image": "https://raw.githubusercontent.com/ImamHossainRoni/pykite/main/extras/yellow-kite.png",
+    },
+)
